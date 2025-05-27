93fbd1a9f491e39b0f78af38328bb82453a36618
SumanthNimmagadda <sumanthchowdhary@sumanths-MacBook-Air.local>
2025-05-27 19:52:41 -0400

    Initial commit: Project structure and basic setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace VirtualPetApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Virtual Pet Simulator - Starting...");
+        }
+    }
+}
